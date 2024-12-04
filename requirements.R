1b2205463a50d00730f844f3a734239d0013a5ef
krasniqierenik <erenik.krasniqi@med.uni-heidelberg.de>
2024-12-04 16:27:12 +0100

    added requirements.R

---
+++ requirements.R
@@ -0,0 +1,10 @@
+packages <- c("shiny", "dplyr", "purrr", "readr", "tibble")
+
+install_if_missing <- function(pack) {
+  if (!require(pack, character.only = TRUE)) {
+    install.packages(pack, dependencies = TRUE)
+  }
+}
+
+invisible(sapply(packages, install_if_missing))
+
